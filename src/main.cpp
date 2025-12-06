bfabb8fb34e9c9319c99dfff8b4d4ba8a6f5d03f
Kazuki Tobita <kazukiti201@gmail.com>
2025-12-06 21:27:18 +0900

    Add IMU

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,43 @@
 #include <M5CoreS3.h>
+#include <math.h>
 
 auto& Display = CoreS3.Display;
+auto& Imu = CoreS3.Imu;
 
 void setup() {
   auto cfg = M5.config();
   CoreS3.begin(cfg);
 
   Serial.begin(115200);
-  Serial.println("hello, world!");
 
-  Display.fillScreen(BLACK);
+  Display.fillScreen(TFT_BLACK);
   Display.setTextColor(TFT_WHITE, TFT_BLACK);
   Display.setTextSize(3);
   Display.setCursor(20, 20);
-  Display.println("hello, world!");
+
+  if (!Imu.begin()) {
+    Display.setCursor(0, 0);
+    Display.println("IMU init failed");
+    Serial.println("IMU init failed");
+    while (true) delay(100);
+  }
 }
 
 void loop() {
   CoreS3.update();
-  delay(100);
+
+  float ax, ay, az;
+  Imu.getAccel(&ax, &ay, &az);
+
+  Serial.printf("X: %.3f\n", ax);
+  Serial.printf("Y: %.3f\n", ay);
+  Serial.printf("Z: %.3f\n", az);
+
+  Display.clear();
+  Display.setCursor(0, 0);
+  Display.printf("X: %.3f g\n", ax);
+  Display.printf("Y: %.3f g\n", ay);
+  Display.printf("Z: %.3f g\n", az);
+
+  delay(200);
 }
