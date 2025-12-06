633d2bdf5630f8150c47bc6919648ae385964d89
Kazuki Tobita <kazukiti201@gmail.com>
2025-12-06 20:56:00 +0900

    Add code template

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <M5CoreS3.h>
+
+auto& Display = CoreS3.Display;
+
+void setup() {
+  auto cfg = M5.config();
+  CoreS3.begin(cfg);
+
+  Display.fillScreen(BLACK);
+  Display.setTextColor(TFT_WHITE, TFT_BLACK);
+  Display.setTextSize(3);
+  Display.setCursor(20, 20);
+  Display.println("hello, world!");
+}
+
+void loop() {
+  CoreS3.update();
+  delay(100);
+}
